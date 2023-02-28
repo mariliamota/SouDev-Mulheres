efdf6f15498dfe2bb6c2e4a13be05b8bd5480701
vitormelov <vitormelov@gmail.com>
2023-02-28 12:11:09 -0300

    atualizado deletaraluno

---
+++ Aula#8/alunos/js/alunoController.js
@@ -0,0 +1,57 @@
+//READ
+function listarAlunos(){
+
+    let alunos = JSON.parse(localStorage.getItem('alunos')) || [];
+
+    let tabela = document.getElementById("tabela-body");
+    tabela.innerHTML = "";
+    alunos.forEach((aluno, key) => {
+        tabela.innerHTML += `<tr>
+                                <td>${key}</td>
+                                <td>${aluno.nome}</td>
+                                <td>${aluno.email}</td>
+                                <td>${aluno.telefone}</td>
+                                <td>${aluno.cidade}</td>
+                                <td>
+                                    <button class="btn btn-outline-warning">Editar</button>
+                                    <button class="btn btn-outline-danger" onclick="deletarAluno(${key})">Excluir</button>
+                                </td>
+                            </tr>`;
+    })
+}
+
+//CREATE
+function salvarAluno(){
+    event.preventDefault();
+    let alunos = JSON.parse(localStorage.getItem('alunos')) || [];
+    let aluno = {
+        nome: nome.value,
+        email: email.value,
+        telefone: tel.value,
+        cidade: cidade.value
+    }
+    alunos.push(aluno);
+    localStorage.setItem('alunos', JSON.stringify(alunos));
+    alert('Adicionado com Sucesso!');
+}
+
+//UPDATE
+function atualiazarAkuno(){}
+
+//DELETE
+function deletarAluno(id){
+    let alunos = JSON.parse(localStorage.getItem('alunos')) || [];
+    let alunosAtualizados = [];
+
+    alunos.forEach((aluno, key) => {
+        if(id != key){
+            alunosAtualizados.push(aluno);
+        }
+    });
+
+    localStorage.setItem('alunos', JSON.stringify(alunosAtualizados));
+
+    alert('Excluido');
+
+    listarAlunos();
+}
--- Aula#8/alunos/js/darkmode.js
+++ Aula#8/alunos/js/darkmode.js
@@ -25,97 +25,68 @@ function ativarDarkmode(){
 }
 
 //////////////////////////////////ARRAY DE OBJETOS///////////////////////////////////////////////
-let alunos = [
-    {
-        id: 1,
-        nome: 'Francisco',
-        email: 'francisco@gmail.com',
-        telefone: '00 00000-0000',
-        cidade: 'Fortaleza-CE'
-    },
-    {
-        id: 2,
-        nome: 'Sarah Camilo',
-        email: 'sarah@gmail.com',
-        telefone: '00 00000-0000',
-        cidade: 'Fortaleza-CE'
-    },
-    {
-        id: 3,
-        nome: 'Samanthan Kuran',
-        email: 'samanthank@gmail.com',
-        telefone: '00 00000-0000',
-        cidade: 'Fortaleza-CE'
-    },
-    {
-        id: 4,
-        nome: 'Ana',
-        email: 'ana@gmail.com',
-        telefone: '00 00000-0000',
-        cidade: 'Maranguape-CE'
-    },
-    {
-        id: 5,
-        nome: 'João',
-        email: 'jao@gmail.com',
-        telefone: '00 00000-0000',
-        cidade: 'Maracanau-CE'
-    },
-    {
-        id: 6,
-        nome: 'Marília',
-        email: 'marilia.mota90@gmail.com',
-        telefone: '00 00000-0000',
-        cidade: 'Maracanau-CE'
-    },
-    {
-        id: 7,
-        nome: 'Maria Mariana',
-        email: 'mariananasce7@gmail.com',
-        telefone: '00 00000-0000',
-        cidade: 'Maracanau-CE'
-    },
-    {
-        id: 8,
-        nome: 'Ana Kelly',
-        email: 'anakellymourab@gmail.com',
-        telefone: '00 00000-0000',
-        cidade: 'Caucaia-CE'
-    },
-    {
-        id: 9,
-        nome: 'Lane Gomes',
-        email: 'lane.gomes@gmail.com',
-        telefone: '00 00000-0000',
-        cidade: 'Fortaleza-CE'
-    },
-];
-
-function listarAlunos(){
-    let tabela = document.getElementById("tabela-body");
-    
-    alunos.forEach((aluno) => {
-        tabela.innerHTML += `<tr>
-                                <td>${aluno.id}</td>
-                                <td>${aluno.nome}</td>
-                                <td>${aluno.email}</td>
-                                <td>${aluno.telefone}</td>
-                                <td>${aluno.cidade}</td>
-                                <td>
-                                    <button class="btn btn-outline-warning">Editar</button>
-                                    <button class="btn btn-outline-danger">Excluir</button>
-                                </td>
-                            </tr>`;
-    })
-}
-
-function salvarAluno(){
-    event.preventDefault();
-    let aluno = {
-        nome: nome.value,
-        email: email.value,
-        telefone: tel.value,
-        cidade: cidade.value
-    }
-    alunos.push(aluno)
-}
+// let alunos = [
+//     {
+//         id: 1,
+//         nome: 'Francisco',
+//         email: 'francisco@gmail.com',
+//         telefone: '00 00000-0000',
+//         cidade: 'Fortaleza-CE'
+//     },
+//     {
+//         id: 2,
+//         nome: 'Sarah Camilo',
+//         email: 'sarah@gmail.com',
+//         telefone: '00 00000-0000',
+//         cidade: 'Fortaleza-CE'
+//     },
+//     {
+//         id: 3,
+//         nome: 'Samanthan Kuran',
+//         email: 'samanthank@gmail.com',
+//         telefone: '00 00000-0000',
+//         cidade: 'Fortaleza-CE'
+//     },
+//     {
+//         id: 4,
+//         nome: 'Ana',
+//         email: 'ana@gmail.com',
+//         telefone: '00 00000-0000',
+//         cidade: 'Maranguape-CE'
+//     },
+//     {
+//         id: 5,
+//         nome: 'João',
+//         email: 'jao@gmail.com',
+//         telefone: '00 00000-0000',
+//         cidade: 'Maracanau-CE'
+//     },
+//     {
+//         id: 6,
+//         nome: 'Marília',
+//         email: 'marilia.mota90@gmail.com',
+//         telefone: '00 00000-0000',
+//         cidade: 'Maracanau-CE'
+//     },
+//     {
+//         id: 7,
+//         nome: 'Maria Mariana',
+//         email: 'mariananasce7@gmail.com',
+//         telefone: '00 00000-0000',
+//         cidade: 'Maracanau-CE'
+//     },
+//     {
+//         id: 8,
+//         nome: 'Ana Kelly',
+//         email: 'anakellymourab@gmail.com',
+//         telefone: '00 00000-0000',
+//         cidade: 'Caucaia-CE'
+//     },
+//     {
+//         id: 9,
+//         nome: 'Lane Gomes',
+//         email: 'lane.gomes@gmail.com',
+//         telefone: '00 00000-0000',
+//         cidade: 'Fortaleza-CE'
+//     },
+// ];
