94d02568bbe0af16ac556b13815e19761c6ab6e8
mrcsporto <marilia.mota90@gmail.com>
2023-03-07 11:51:55 -0300

    modificações alunoController.js

--- Aula#8/alunos/js/alunoController.js
+++ Aula#8/alunos/js/alunoController.js
@@ -2,7 +2,7 @@
 function listarAlunos(){
 
     // let alunos = JSON.parse(localStorage.getItem('alunos')) || [];
-    let alunos = [];
+
     fetch("http://localhost:8000/alunos")
     .then(res => res.json())
     .then(alunos => {
@@ -10,14 +10,14 @@ function listarAlunos(){
         tabela.innerHTML = "";
         alunos.forEach((aluno, key) => {
             tabela.innerHTML += `<tr>
-                                    <td>${key}</td>
+                                    <td>${aluno.id}</td>
                                     <td>${aluno.nome}</td>
                                     <td>${aluno.email}</td>
                                     <td>${aluno.telefone}</td>
                                     <td>${aluno.cidade}</td>
                                     <td>
                                         <button class="btn btn-outline-warning">Editar</button>
-                                        <button class="btn btn-outline-danger" onclick="deletarAluno(${key})">Excluir</button>
+                                        <button class="btn btn-outline-danger" onclick="deletarAluno(${aluno.id})">Excluir</button>
                                     </td>
                                 </tr>`;
         })
@@ -27,35 +27,52 @@ function listarAlunos(){
 //CREATE
 function salvarAluno(){
     event.preventDefault();
-    let alunos = JSON.parse(localStorage.getItem('alunos')) || [];
+    // let alunos = JSON.parse(localStorage.getItem('alunos')) || [];
     let aluno = {
         nome: nome.value,
         email: email.value,
         telefone: tel.value,
         cidade: cidade.value
     }
-    alunos.push(aluno);                                                     //array.push adiciona no final do array o proximo item
-    localStorage.setItem('alunos', JSON.stringify(alunos));
-    alert('Adicionado com Sucesso!');
+    fetch("http://localhost:8000/alunos", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(aluno)
+    })
+    .then(res => res.json())
+    .then(resposta => {
+        alert('Adicionado com Sucesso!');
+        console.log(resposta)
+    })
+    // alunos.push(aluno);                                                     //array.push adiciona no final do array o proximo item
+    // localStorage.setItem('alunos', JSON.stringify(alunos));
 }
 
 //UPDATE
-function atualiazarAkuno(){}
+function atualiazarAluno(){}
 
 //DELETE
 function deletarAluno(id){
-    let alunos = JSON.parse(localStorage.getItem('alunos')) || [];
-    let alunosAtualizados = [];
+    // let alunos = JSON.parse(localStorage.getItem('alunos')) || [];
+    // let alunosAtualizados = [];
 
-    alunos.forEach((aluno, key) => {
-        if(id != key){
-            alunosAtualizados.push(aluno);
-        }
-    });
+    // alunos.forEach((aluno, key) => {
+    //     if(id != key){
+    //         alunosAtualizados.push(aluno);
+    //     }
+    // });
 
-    localStorage.setItem('alunos', JSON.stringify(alunosAtualizados));
+    // localStorage.setItem('alunos', JSON.stringify(alunosAtualizados));
 
-    alert('Excluido');
-
-    listarAlunos();
+    fetch(`http://localhost:8000/alunos/${id}`, {
+        method:"DELETE",
+    })
+    .then(res => res.json())
+    .then(resposta => {
+        alert('Excluido com sucesso!');
+        console.log(resposta)
+        listarAlunos();
+    })
 }
