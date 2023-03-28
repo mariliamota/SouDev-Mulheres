0d0d6effad03a8edca4681b087c1386740a2f353
mrcsporto <marilia.mota90@gmail.com>
2023-03-28 10:01:46 -0300

    aula 40 de BD

---
+++ Aula#40/comandos.sql
@@ -0,0 +1,39 @@
+-- Para fazer atualização de um registro (nunca esquecer de usar o WHERE) --
+UPDATE tb_aluno SET
+    matricula='1234567'
+WHERE
+    id='8'; --(a pessoa q tiver o id=8 receberá a nova matricula 1234567)--
+____________________________________________________________________________________________________
+-- | Antes de fazer um     | --  
+-- | delete fazer um       | --
+-- | select                | --
+-- |_______________________| --
+____________________________________________________________________________________________________
+-- criando uma nova tabela --
+CREATE TABLE tb_escola (
+    id INT  AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    endereco VARCHAR(100)
+);
+____________________________________________________________________________________________________
+-- inserindo dados na tabela --
+INSERT INTO tb_escola
+    (nome, endereco)
+VALUES
+    ('Digital College Sul', 'Av. Washington Soares, 3663 - Edson Queiroz');
+____________________________________________________________________________________________________
+-- modificar as escolas do aluno --
+UPDATE tb_aluno SET
+escola_id=3 -- quem tem a escola igual 3 --
+WHERE
+escola_id=30; -- terá uma nova escola equivalente a 30 --
+--outra forma--
+UPDATE tb_aluno SET 
+escola_id=1
+BETWEEN 20 AND 21 -- o between é tudo entre 20 e 21 --
+--segunda forma--
+UPDATE tb_aluno SET
+escola_id=2
+WHERE
+escola_id=12 OR escola_id=13 OR escola_id=17
+____________________________________________________________________________________________________
