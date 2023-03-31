fc89bb001b7e98593f7bc6ffa667ec67ef3fa210
mrcsporto <marilia.mota90@gmail.com>
2023-03-31 10:55:44 -0300

    tabela categorias e verbos http

--- projeto-api/resources/category/routes.js
+++ projeto-api/resources/category/routes.js
@@ -6,4 +6,27 @@ app.get("/categories", async (req, res) => {
     res.send(await controller.listAll());
 });
 
+app.get("/categories/:id", async (req, res) => {
+    res.send(await controller.listOne(req.params.id));
+})
+
+//rota para criar uma linha na tabela
+app.post("/categories", async (req, res) => {
+    res.send(await controller.create(req.body));
+});
+
+//endpoint para editar uma linha
+app.put("/categories/:id", async (req, res) => {
+    res.send(await controller.edit(req.params.id, req.body));
+});
+
+app.patch("/categories/:id", async (req, res) => {
+    res.send(await controller.disable(req.params.id));
+})
+
+//apagar
+app.delete("/categories/:id", async (req, res) => {
+    res.send(await controller.destroy(req.params.id));
+});
+
 module.exports = app;
