8a95306c229f3d93fcafc4e4891ce80ec5504196
mrcsporto <marilia.mota90@gmail.com>
2023-04-11 11:56:16 -0300

    testes automatizados com cypress

--- aula44/cypress/e2e/teste.cep.cy.js
+++ aula44/cypress/e2e/teste.cep.cy.js
@@ -2,4 +2,12 @@ describe('Testar pagina cep', () => {
     beforeEach(() => {
         cy.visit('https://alessandrofeitoza.github.io/endereco-via-cep/')
     });
+
+    it('buscar o cep', () => {
+        cy.get('#cep').type("60416510").blur()
+        cy.wait(2000)
+        cy.get('#logradouro').should('have.value', 'Rua Raul Uchôa')
+        cy.get('#bairro').should('have.value', 'Bom Futuro')
+        cy.get('#cidade').should('have.value', 'Fortaleza')
+    })
 });
